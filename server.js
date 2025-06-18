bec312abf3960cd095e84699e922561714a2a502
Kumar Pratik <kumarpratik1006@gmail.com>
2025-06-18 11:36:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -11,15 +12,41 @@ const io = new Server(server, {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+let roomUsers = {}; // Room: Set of socket ids
+
 io.on('connection', socket => {
+  let currentRoom = '';
+
   socket.on('join-room', room => {
+    if (currentRoom) {
+      socket.leave(currentRoom);
+      roomUsers[currentRoom]?.delete(socket.id);
+      updateUserCount(currentRoom);
+    }
+    currentRoom = room;
     socket.join(room);
+
+    if (!roomUsers[room]) roomUsers[room] = new Set();
+    roomUsers[room].add(socket.id);
+    updateUserCount(room);
   });
 
-  socket.on('send-message', data => {
-    io.to(data.room).emit('receive-message', data);
+  socket.on('send-message', ({ room, message }) => {
+    socket.to(room).emit('receive-message', message);
+  });
+
+  socket.on('disconnect', () => {
+    if (currentRoom) {
+      roomUsers[currentRoom]?.delete(socket.id);
+      updateUserCount(currentRoom);
+    }
   });
 });
+
+function updateUserCount(room) {
+  const count = roomUsers[room]?.size || 0;
+  io.to(room).emit('update-users', count);
+}
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
