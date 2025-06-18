15ec36ea92a8e1a5e4fd742f47aadde7e91b3b52
Kumar Pratik <kumarpratik1006@gmail.com>
2025-06-18 12:03:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: '*' }
+  cors: { origin: '*' },
+  pingTimeout: 60000, // 60 sec timeout to prevent auto disconnect
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
